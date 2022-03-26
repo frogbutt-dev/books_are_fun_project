d400021d14c03bda8a40b8e641d6e21fba244808
Ali Zafar <2557683z@student.gla.ac.uk>
2022-03-26 14:45:39 +0000

    added search API and user profile stuff

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 from rango.models import Book, Review
-from rango.forms import BookForm, ReviewForm
+from rango.forms import BookForm, ReviewForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from rango.bing_search import run_query
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
+from rango.models import UserProfile
+
 
 
 def index(request):
@@ -273,3 +279,116 @@ def visitor_cookie_handler(request):
 
     # Update/set the visits cookie
     request.session['visits'] = visits
+
+
+def search(request):
+    context_dict = {}
+    if request.method == 'POST':
+        if request.method == 'POST':
+            query = request.POST['query'].strip()
+
+            if query:
+                context_dict['result_list'] = run_query(query)
+                context_dict['query'] = query
+    return render(request, 'rango/search.html', context=context_dict)
+
+
+@login_required
+def register_profile(request):
+    form = UserProfileForm()
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            user_profile = form.save(commit=False)
+            user_profile.user = request.user
+            user_profile.save()
+            return redirect(reverse('rango:index'))
+        else:
+            print(form.errors)
+    
+    context_dict = {'form': form}
+    return render(request, 'rango/profile_registration.html', context_dict)
+
+
+class AboutView(View):
+    def get(self, request):
+        context_dict = {}
+        
+        visitor_cookie_handler(request)
+        context_dict['visits'] = request.session['visits']
+    
+        return render(request,
+                    'rango/about.html',
+                    context_dict)
+
+
+class AddBookView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        form = BookForm()
+        return render(request, 'rango/add_book.html', {'form': form})
+    
+    @method_decorator(login_required)
+    def post(self, request):
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect(reverse('rango:index'))
+        else:
+            print(form.errors)
+            return render(request, 'rango/add_book.html', {'form': form})
+
+
+class ProfileView(View):
+    def get_user_details(self, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+        
+        user_profile = UserProfile.objects.get_or_create(user=user)[0]
+        form = UserProfileForm({'picture': user_profile.picture,})
+        
+        return (user, user_profile, form)
+    
+    @method_decorator(login_required)
+    def get(self, request, username):
+        try:
+            (user, user_profile, form) = self.get_user_details(username)
+        except TypeError:
+            return redirect(reverse('rango:index'))
+        
+        context_dict = {'user_profile': user_profile,
+                        'selected_user': user,
+                        'form': form}
+        
+        return render(request, 'rango/profile.html', context_dict)
+    
+    @method_decorator(login_required)
+    def post(self, request, username):
+        try:
+            (user, user_profile, form) = self.get_user_details(username)
+        except TypeError:
+            return redirect(reverse('rango:index'))
+        
+        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('rango:profile', user.username)
+        else:
+            print(form.errors)
+        
+        context_dict = {'user_profile': user_profile,
+                        'selected_user': user,
+                        'form': form}
+        
+        return render(request, 'rango/profile.html', context_dict)
+
+class ListProfilesView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        profiles = UserProfile.objects.all()
+        return render(request,
+                        'rango/list_profiles.html',
+                        {'userprofile_list': profiles})
