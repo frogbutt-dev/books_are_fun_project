4cbd281956791e6b25a39ce4a264745bd776f111
Stefan Goncalves <2578879g@student.gla.ac.uk>
2022-03-28 09:46:04 +0100

    turned off debug mode

--- books_are_fun_project/settings.py
+++ books_are_fun_project/settings.py
@@ -23,10 +23,14 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'on99is8o965nl#bql247z*76wx28gd52hc%6h2-ruj-u_cs31_'
+key = None
+with open('secret.key') as f:
+    key = f.read().strip()
+
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['booksarefun.pythonanywhere.com']
 
